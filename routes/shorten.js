0b9c6e09e1133daf403f355d28aa5515b078c534
Jayant Dhawan <jayantdhawan95@gmail.com>
2020-03-03 17:12:08 +0530

    fixed short url link

--- routes/shorten.js
+++ routes/shorten.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const shortid = require('shortid');
 const validURL = require('valid-url');
 const redirectSchema = require('../models/redirects');
-var baseURL = process.env.baseURL || "http://localhost:5000/";
+var baseURL = process.env.BASEURL || "http://localhost:5000/";
 
 /* Handle GET Request */
 router.get('/', (req, res) => {
